61684696e72de598dc0b63933491559b0bd7747d
Lennon Rubin <lprubin.lennon@gmail.com>
2014-05-02 15:03:41 -0400

    styling of circle CTA animation

--- source/js/draw.js
+++ source/js/draw.js
@@ -11,17 +11,16 @@ var pathObj = {
     "newsletter-circle": {
         "strokepath": [
             {
-                "path": "M132.7,1.2c6.3-0.5,12.8-0.8,19.3-0.8c83.9,0,152,43.4,152,97  s-68.1,97-152,97S0,150.9,0,97.4C0,48,57.8,7.2,132.7,1.2",
-                "duration": 600
+                "path": "M129.5,4.5c6.3-0.5,25-0.1,31.5-0.1c83.9,0,149.7,46.3,149.7,99.9  s-68.1,97-152,97s-152-43.4-152-97C6.7,55,65.1,18.1,140,12",
+                "duration": 1000
             }
         ],
         "dimensions": {
-            "width": 314,
-            "height": 250
+            "width": 323,
+            "height": 276
         }
     }
 }; 
- 
  
 /* 
  Setup and Paint your lazyline! 
@@ -29,11 +28,12 @@ var pathObj = {
 
  
 $(".newsletter-signup").click(function(e){
-	 e.preventDefault() 
+	 e.preventDefault()
+	 var strokeColor = $("#newsletter-circle").css("color")
 	 $('#newsletter-circle').lazylinepainter( 
 	 {
 	    "svgData": pathObj,
-	    "strokeWidth": 2,
-	    "strokeColor": "#e09b99"
+	    "strokeWidth": 3,
+	    "strokeColor": strokeColor
 	}).lazylinepainter('paint'); 
  });
