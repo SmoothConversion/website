7b00bc9d55ab18a7f9ceb4ac124dbb1c17afc72a
Dane Schneider <dane.schneider@gmail.com>
2014-07-15 11:40:12 -0400

    lots of work on responsiveness and general polish

--- source/js/draw.js
+++ source/js/draw.js
@@ -12,7 +12,7 @@ var pathObj = {
         "strokepath": [
             {
                 "path": "M129.5,4.5c6.3-0.5,25-0.1,31.5-0.1c83.9,0,149.7,46.3,149.7,99.9  s-68.1,97-152,97s-152-43.4-152-97C6.7,55,65.1,18.1,140,12",
-                "duration": 1000
+                "duration": 800
             }
         ],
         "dimensions": {
@@ -29,6 +29,10 @@ var pathObj = {
  
 $(".newsletter-signup").click(function(e){
 	 e.preventDefault()
+
+     $("form.subscribe input.email").focus();
+     if ($(window).scrollLeft() != 0) $(window).animate({scrollLeft: 0}, 400);
+
 	 var strokeColor = $("#newsletter-circle").css("color")
 	 $('#newsletter-circle').lazylinepainter( 
 	 {
